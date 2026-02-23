78330a629e8e94348969b8a936bf5ace1f1d3a01
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-23 08:21:28 +0530

    feat: add place tool and context menu

--- gui/windows/widgets/editor/editor.py
+++ gui/windows/widgets/editor/editor.py
@@ -2,149 +2,213 @@ from enum import Enum
 from math import isclose
 from typing import override
 from PySide6 import QtGui
-from PySide6.QtCore import QPoint, QPointF, QRect, QRectF, Qt
-from PySide6.QtGui import QKeyEvent, QPainter, QPainterPath, QPen, Qt
+from PySide6.QtCore import QPointF, QRect, QRectF, Qt, Signal
+from PySide6.QtGui import (
+    QAction,
+    QContextMenuEvent,
+    QKeyEvent,
+    QPainter,
+    QPainterPath,
+    QPen,
+    Qt,
+)
 from PySide6.QtWidgets import (
     QGraphicsItem,
     QGraphicsScene,
+    QGraphicsSceneContextMenuEvent,
     QGraphicsSceneMouseEvent,
     QGraphicsView,
+    QMenu,
 )
+from quantsim.core import Qubit
 
 from .caditem import CADItem
 from .qubit import QubitCADItem
 
+
 class Tools(Enum):
     NONE = 0
     WIRE = 1
+    PLACE = 3
+
+
+class Editor(QGraphicsView): ...
 
 
 class GraphicsCanvas(QGraphicsScene):
     previous_click: QPointF
     current_pos: QPointF
+    editor: Editor
+    selected_caditem: CADItem | None = None
     temp_paths: list[QGraphicsItem] = []
-    grid: list[int] = []
-
+    temp_caditems: list[CADItem] = []
     wires: list[QGraphicsItem] = []
-
     cad_items: list[CADItem] = []
     first_press: bool = True
 
-
-    def __init__(self, tool: Tools, grid = []) -> None:
-        self.tool: Tools = tool
+    def __init__(self, editor: Editor) -> None:
         super().__init__()
-
+        self.editor = editor
         self.setBackgroundBrush(Qt.GlobalColor.white)
-        self.grid = [1200, 800]
-        self.add_caditem(QubitCADItem())    
+        self.add_caditem(QubitCADItem())
 
     def add_caditem(self, item: CADItem) -> None:
         self.addItem(item)
         self.cad_items.append(item)
-    
-    def set_tool(self, tool: Tools):
-        self.tool = tool
-    
 
     def snap(self, pos: QPointF) -> QPointF:
-
         for caditem in self.cad_items:
-            inputpos = caditem.input_position
-            if inputpos and isclose(inputpos.x(), pos.x()) and isclose(inputpos.y(), pos.y(), rel_tol=1e-1):
+            _pos = caditem.input
+            if (
+                _pos
+                and isclose(_pos.x(), pos.x(), rel_tol=1e-1)
+                and isclose(_pos.y(), pos.y(), rel_tol=1e-1)
+            ):
+                return _pos
+            _pos = caditem.output
+            if (
+                _pos
+                and isclose(_pos.x(), pos.x(), rel_tol=1e-1)
+                and isclose(_pos.y(), pos.y(), rel_tol=1e-1)
+            ):
+                return _pos
 
-                return inputpos
-            outputpos = caditem.output_position
-            if outputpos and isclose(outputpos.x(), pos.x()) and isclose(outputpos.y(), pos.y(), rel_tol=1e-1):
-                return outputpos
-            
         point = pos.toPoint()
-        if point.x()  % 100 < 15:
-            point.setX(point.x() - point.x() % 100) 
-        
-        if point.y()  % 100 < 15:
+        if point.x() % 100 < 15:
+            point.setX(point.x() - point.x() % 100)
+
+        if point.y() % 100 < 15:
             point.setY(point.y() - point.y() % 100)
 
         return point.toPointF()
 
+    @override
+    def contextMenuEvent(self, event: QGraphicsSceneContextMenuEvent, /) -> None:
+        for item in self.cad_items:
+            if item.contains(item.mapFromScene(event.scenePos())):
+                menu = QMenu(title="Editor menu")
+                menu.addAction(QAction(parent=self, text="Connect"))
+                _ = menu.exec(event.screenPos())
 
     @override
     def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent, /) -> None:
-        path = QPainterPath()
-
-        for _ in self.temp_paths:
+        # remove temp items
+        for _ in range(len(self.temp_paths)):
             self.removeItem(self.temp_paths.pop())
-            
-        if not self.first_press:
-            path.moveTo(self.snap(self.previous_click))
-            pos = self.snap(event.scenePos())
-            if abs(pos.x()) >= abs(pos.y()):
-                path.lineTo(QPointF(pos.x(), self.previous_click.y()))
-                path.moveTo(QPointF(pos.x(), self.previous_click.y()))
-                path.lineTo(QPointF(pos.x(),pos.y()))
-            else:
-                path.lineTo(QPointF(self.previous_click.x(), pos.y()))
-                path.moveTo(QPointF(self.previous_click.x(), pos.y()))
-                path.lineTo(QPointF(pos.x(), pos.y()))
-            
 
-            self.temp_paths.append(self.addPath(path))
+        for _ in range(len(self.temp_caditems)):
+            self.removeItem(self.temp_caditems.pop())
+
+        print(self.temp_caditems)
+        match self.editor.tool:
+            case Tools.WIRE:
+                path = QPainterPath()
+
+                if not self.first_press:
+                    path.moveTo(self.snap(self.previous_click))
+                    pos = self.snap(event.scenePos())
+                    if abs(pos.x()) >= abs(pos.y()):
+                        path.lineTo(QPointF(pos.x(), self.previous_click.y()))
+                        path.moveTo(QPointF(pos.x(), self.previous_click.y()))
+                        path.lineTo(QPointF(pos.x(), pos.y()))
+                    else:
+                        path.lineTo(QPointF(self.previous_click.x(), pos.y()))
+                        path.moveTo(QPointF(self.previous_click.x(), pos.y()))
+                        path.lineTo(QPointF(pos.x(), pos.y()))
+
+                    self.temp_paths.append(self.addPath(path))
+
+            case Tools.PLACE:
+                self.first_press = True
+                if self.editor.cadItem:
+                    self.editor.cadItem.setPos(
+                        QPointF(
+                            event.scenePos().toPoint().x() - 32,
+                            event.scenePos().toPoint().y() - 32,
+                        )
+                    )
+                    self.addItem(self.editor.cadItem)
+                    self.temp_caditems.append(self.editor.cadItem)
+                    self.editor.cadItem = QubitCADItem()
+
+            case Tools.NONE:
+                pass
 
     @override
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent, /) -> None:
-        if self.tool == Tools.WIRE:
-            if self.first_press:
-                self.previous_click = event.scenePos()
-                self.first_press = False
-            else:
-                path = QPainterPath()
-                path.moveTo(self.snap(self.previous_click))
-                pos = self.snap(event.scenePos())
-                if abs(pos.x()) >= abs(pos.y()):
-                    path.lineTo(QPointF(pos.x(), self.previous_click.y()))
-                    path.moveTo(QPointF(pos.x(), self.previous_click.y()))
-                    path.lineTo(QPointF(pos.x(), pos.y()))
+        match self.editor.tool:
+            case Tools.WIRE:
+                if self.first_press:
+                    self.previous_click = event.scenePos()
+                    self.first_press = False
+
                 else:
-                    path.lineTo(QPointF(self.previous_click.x(), pos.y()))
-                    path.moveTo(QPointF(self.previous_click.x(), pos.y()))
-                    path.lineTo(QPointF(pos.x(), pos.y()))                
-                self.wires.append(self.addPath(path))
-                self.first_press = True
-    
+                    path = QPainterPath()
+                    path.moveTo(self.snap(self.previous_click))
+                    pos = self.snap(event.scenePos())
+                    if abs(pos.x()) >= abs(pos.y()):
+                        path.lineTo(QPointF(pos.x(), self.previous_click.y()))
+                        path.moveTo(QPointF(pos.x(), self.previous_click.y()))
+                        path.lineTo(QPointF(pos.x(), pos.y()))
+                    else:
+                        path.lineTo(QPointF(self.previous_click.x(), pos.y()))
+                        path.moveTo(QPointF(self.previous_click.x(), pos.y()))
+                        path.lineTo(QPointF(pos.x(), pos.y()))
+                    self.wires.append(self.addPath(path))
+                    self.first_press = True
+
+            case Tools.PLACE:
+                if self.editor.cadItem:
+                    self.editor.cadItem.setPos(
+                        QPointF(
+                            event.scenePos().toPoint().x() - 32,
+                            event.scenePos().toPoint().y() - 32,
+                        )
+                    )
+                    self.add_caditem(self.editor.cadItem)
+                    self.editor.cadItem = QubitCADItem()
+
+            case Tools.NONE:
+                for item in self.cad_items:
+                    if item.contains(item.mapFromScene(event.scenePos())):
+                        self.selected_caditem = item
+
+                        if isinstance(self.selected_caditem, QubitCADItem):
+                            self.editor.qubititem_selected.emit(
+                                self.selected_caditem.qubit
+                            )
+
     @override
     def keyReleaseEvent(self, event: QKeyEvent, /) -> None:
         pass
-
-    
 
 
 class Editor(QGraphicsView):
     """
     Custom editor widget which draws all the symbols and connections
     """
+
     tool: Tools = Tools.NONE
     cadItem: CADItem | None = None
     grid_step: int = 100
     grid_pen: QPen = QPen(Qt.GlobalColor.lightGray)
-
-
+    qubititem_selected: Signal = Signal(Qubit)
     _scene: GraphicsCanvas
 
     def __init__(self):
-        self.tool: Tools = Tools.WIRE
-        self._scene = GraphicsCanvas(self.tool)
-        _ = self._scene.changed.connect(self.scene_changed)
-        
+        self._scene = GraphicsCanvas(self)
+
         super().__init__(self._scene)
         self.setGeometry(QRect(0, 0, 800, 600))
         self.setRenderHint(QPainter.RenderHint.Antialiasing)
         self.setMouseTracking(True)
 
-        
-    
+    def set_tool(self, tool: Tools) -> None:
+        self.tool = tool
+
     @override
     def drawBackground(self, painter: QtGui.QPainter, rect: QRectF | QRect, /) -> None:
-        painter.translate(.5, .5)
+        painter.translate(0.5, 0.5)
         painter.setPen(self.grid_pen)
         painter.fillRect(rect, Qt.GlobalColor.white)
 
@@ -157,7 +221,7 @@ class Editor(QGraphicsView):
 
         top = y
         left = x
-        step  = self.grid_step
+        step = self.grid_step
 
         yrest = y % step
         if yrest:
@@ -170,13 +234,3 @@ class Editor(QGraphicsView):
             x += step - xrest
         for x in range(x, right, step):
             painter.drawLine(x, top, x, bottom)
-
-
-
-    def scene_changed(self):
-        # do something
-        pass
-
-
-    
-        
