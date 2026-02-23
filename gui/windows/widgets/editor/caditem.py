4cb9a56db70f4f7f21a334805a89099fc26c534f
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-23 08:22:16 +0530

    fix: add input and output items to caditem

--- gui/windows/widgets/editor/caditem.py
+++ gui/windows/widgets/editor/caditem.py
@@ -1,13 +1,25 @@
-from PySide6.QtCore import QPointF, QSize
+from PySide6.QtCore import QSize, QPointF
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QGraphicsPixmapItem
 
 
-class CADItem(QGraphicsPixmapItem):
-    input_position: QPointF | None
-    output_position: QPointF | None
+class CADItem(QGraphicsPixmapItem): ...
 
-    def __init__(self, path: str, input_position: QPointF | None, output_postion: QPointF | None) -> None:
+
+class CADItem(QGraphicsPixmapItem):
+    input: QPointF | None
+    output: QPointF | None
+
+    inputItems: list[CADItem] | None = None
+    ouputItems: list[CADItem] | None = None
+
+    def __init__(
+        self, path: str, input: QPointF | None, output: QPointF | None
+    ) -> None:
         super().__init__(QPixmap(path).scaled(QSize(64, 64)))
-        self.input_position = input_position
-        self.output_position = output_postion
+
+        self.input = self.mapToScene(input) if input is not None else None
+        self.output = self.mapToScene(output) if output is not None else None
+
+        print(self.input)
+        print(self.output)
